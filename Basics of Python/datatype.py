19a5a01dd86c1fe9d735c6b3d68028ba74f0a4c6
krishp285 <krishkpatelk2005@gmail.com>
2025-06-24 10:45:15 +0530

    first commit

---
+++ Basics of Python/datatype.py
@@ -0,0 +1,9 @@
+# Program to check data type of a variable
+
+a = input("Enter a value: ")
+print("The value is:", a)
+print("The type of the value is:", type(a))
+
+b = int(input("Enter an integer value: "))
+print("The value is:", b)
+print("The type of the value is:", type(b))
---
+++ Basics of Python/input_type_print.py
@@ -0,0 +1,8 @@
+# Program to demonstrate use of input(), type(), and print()
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print("Hello", name, "your age is", age)
+
+print("Type of name:", type(name))
+print("Type of age:", type(age))
