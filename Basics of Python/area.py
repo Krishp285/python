3b8722dcd8b0fa11890da41eb5280fbe5013947c
krishp285 <krishkpatelk2005@gmail.com>
2025-07-04 17:51:04 +0530

    first commit

--- Basics of Python/area.py
+++ Basics of Python/area.py
@@ -4,3 +4,9 @@ radius = float(input("Enter the radius of the circle: "))
 area = 3.14 * radius * radius
 
 print("The area of the circle is:", area)
+
+# Program to calculate the area of a circle using a math library
+import math
+radius = float(input("Enter the radius of the circle: "))
+area = math.pi * radius * radius
+print("The area of the circle is:", area)
