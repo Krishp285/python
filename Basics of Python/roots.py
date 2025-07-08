8bbd4b11ea30ffdcb83e21067ddd5521905e7c94
krishp285 <krishkpatelk2005@gmail.com>
2025-07-08 09:09:19 +0530

    first commit

---
+++ Basics of Python/roots.py
@@ -0,0 +1,7 @@
+# write a program to find square root and cube root 
+
+import math
+
+a = int(input("enter value to find square root and cube root :-"));
+print(math.sqrt(a));
+print(math.cbrt(a));
